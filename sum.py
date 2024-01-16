f62133a87841ba9f8a7f299f414418f4cf1e7814
MohammadNauman <mirzanauman1502@outlook.com>
2024-01-16 13:59:08 +0500

    Second change of the code

--- sum.py
+++ sum.py
@@ -3,3 +3,10 @@ def sum_numbers(a,b):
 
 result = sum_numbers(5,10)
 print(result)
+
+
+num = int(input("Enter a Number"))
+print("The multiplication of :", num)
+
+for x in range(1,11):
+    print(num , "x" , x , "=" , num * x)
