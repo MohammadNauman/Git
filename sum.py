f54aa30d9956193386111c972eeee5e9c184a311
MohammadNauman <mirzanauman1502@outlook.com>
2024-01-16 16:40:32 +0500

    Minor change

--- sum.py
+++ sum.py
@@ -3,11 +3,14 @@ def sum_numbers(a,b):
     return a + b
 result = sum_numbers(5,10)
 print(result)
-
-
+ 
 
 num = int(input("Enter a Number"))
 print("The multiplication of :", num)
 
 for x in range(1,11):
     print(num , "x" , x , "=" , num * x)
+
+
+#now add a comment in code
+print("Minor change in code")    
