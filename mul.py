d8b9f3e057312150d98a221ac05e782d82fe24c7
MohammadNauman <mirzanauman1502@outlook.com>
2024-01-24 15:36:03 +0500

    Adding multiplcation code

--- mul.py
+++ mul.py
@@ -1,6 +1,17 @@
+#This program will calculate the multiplication of two numbers
+
+#Getting the two numbers for multiplications
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+#Calculating the multiplications
+result = num1 * num2
+
+#Display the result
+print("The multiplication of the two number is :", result)
+
+
 
 fruits = {'apple', 'banana', 'cherry'}
-
 fruits.add('orange') 
-
 print(fruits)
